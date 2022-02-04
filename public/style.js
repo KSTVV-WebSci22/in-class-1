04b80d9839224c737a4a29535d4ec055c50fa36d
Sean Block <42530814+seanblock@users.noreply.github.com>
2022-02-03 21:21:38 -0500

    Finished all sound effects and javascript transitions

--- public/style.js
+++ public/style.js
@@ -1,12 +1,44 @@
 $(document).ready(function(){
 
     console.log("running")
-    $("#chat").hide();
+    $("#dial").hide();
 
-    $("#select" ).click(function() {
+    $("#select").click(function() {
+        
+        // Variable Save
         sessionStorage.setItem("nickname", $('#nickname').val());
+
+        // Login hide, show dial up screen.
         $("#login").hide();
-        $('#chat').css('display', 'flex');
+        $('#dial').show();
+
+        var dialing = document.getElementById("dialing");
+        dialing.play();
+        var buddyin = document.getElementById("buddy-in");
+        var message = document.getElementById("message");
+
+        setTimeout(() => {
+           $("#dial-status").html("Dialing 555-867-5309")
+        }, 2000);
+
+        setTimeout(() => {
+            $("#dial-status").html("Connecting through TCHSHC");
+            $("#connecting-img").show()
+         }, 6000);
+
+         setTimeout(() => {
+            $("#dial-status").html("Connected!!!");
+            $("#connected-img").show()
+         }, 26000);
+
+        setTimeout(() => {
+            // Hide Dial up screen
+            $('#dial').hide();  
+            // Show Chat Screen
+            $('#chat').css('display', 'flex');
+            dialing.pause()
+            buddyin.play()
+        }, 26000);
     });
     
     var nick = sessionStorage.getItem("nickname");
@@ -23,6 +55,9 @@ $(document).ready(function(){
     
 // load socket.io-client
 var socket = io();
+
+
+
 
 function checkInput( str ) {
     let inputs = (str.trim()).split(' ');
@@ -61,34 +96,35 @@ function checkInput( str ) {
     
     // on chat form submit, send msg to server
     $('#chat-form').submit(function(){
-
-    // get nickname from session storage
-    var nick = sessionStorage.getItem("nickname");
         
-    // append and emit message
-    $('#messages').append($('<li>').text(nick + ': ' + $('#message').val()));
-        $('#messages li').last().css("color", "blue");
-    socket.emit('message', nick + ': ' + $('#message').val());
+        // get nickname from session storage
+        var nick = sessionStorage.getItem("nickname");
+            
+        // append and emit message
+        $('#messages').append($('<li>').text(nick + ': ' + $('#message').val()));
+            $('#messages li').last().css("color", "blue");
+        socket.emit('message', nick + ': ' + $('#message').val());
+
+        // Message Sound
+        message.play();
 
 
-    // Checking our text
-    checkInput(document.getElementById("message").value);
+        // Checking our text
+        checkInput(document.getElementById("message").value);
 
-    // Erasing our text
-    $('#message').val('');
+        // Erasing our text
+        $('#message').val('');
 
-    // Button Disable if empty
-    $("#send").prop("disabled",true);
+        // Button Disable if empty
+        $("#send").prop("disabled",true);
 
-    // Scroll to bottom of the page
-    document.getElementById('messages').scrollTo(0,document.getElementById("messages").scrollHeight);
-    return false;
+        // Scroll to bottom of the page
+        document.getElementById('messages').scrollTo(0,document.getElementById("messages").scrollHeight);
+        return false;
     });
     
     // on msg received, append to list
-    socket.on('message', function(msg){
-    $('#messages').append($('<li>').text(msg));
-});
+    
 
 
 //Make the DIV element draggagle:
